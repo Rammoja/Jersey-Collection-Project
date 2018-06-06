49f129c173f3de1d1c7c53edf6dcfe36370c744c
Richard Ramson <richard.ramson.jam@gmail.com>
2018-06-06 12:12:12 +0100

    new rubber duck class

---
+++ My Collection/src/main/java/RubberDuck.java
@@ -0,0 +1,9 @@
+public class RubberDuck extends Item{
+    private String duckType;
+
+    public RubberDuck(String itemType, double buying, double shipping, double marketValue, int month, int year)
+
+
+
+
+}
